17dbbe56424554a72a18c31fae3b9b222e49310c
Ivan Luengo <28649633+luengoi@users.noreply.github.com>
2026-09-06 11:06:12 +0200

    feat: export modules as outputs

--- flake.nix
+++ flake.nix
@@ -16,6 +16,19 @@
   };
 
   outputs = inputs: {
+    homeManagerModules = {
+      default = ./modules/home;
+      aerospace = ./modules/home/programs/aerospace.nix;
+      codex = ./modules/home/programs/codex.nix;
+      ghostty = ./modules/home/programs/ghostty.nix;
+      neovim = ./modules/home/programs/neovim.nix;
+      tmux = ./modules/home/programs/tmux.nix;
+    };
+
+    darwinModules = {
+      defaults = ./modules/darwin/defaults.nix;
+    };
+
     darwinConfigurations.muthur = inputs.nix-darwin.lib.darwinSystem {
       modules = [
         inputs.home-manager.darwinModules.home-manager
